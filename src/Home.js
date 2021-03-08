604b8db1b8e892f796d7f3ccf64377c7885c2d00
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-03-08 22:26:59 +0530

    mask-image

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonSaheli/iwd21/hero/Main-hero-banner-web-7-1X._CB657864478_.jpg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;

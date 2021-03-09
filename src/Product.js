a581d7ecda09ba4fde0291406d3acfaf96eb7240
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-03-09 21:16:28 +0530

    React Context API

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "./Product.css";
-function Product({ title, image, price, rating }) {
+import { useStateValue } from "./StateProvider";
+
+function Product({ id, title, image, price, rating }) {
+  const [state, dispatch] = useStateValue();
+  console.log(state.basket);
+  const addtoBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product_info">
@@ -17,8 +35,8 @@ function Product({ title, image, price, rating }) {
             ))}
         </div>
       </div>
-      <img src={image} alt="" />
-      <button>Add to Basket</button>
+      <img src={image} alt="lol" />
+      <button onClick={addtoBasket}>Add to Basket</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );

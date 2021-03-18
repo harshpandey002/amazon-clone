7ec7619471374d214fb6cdf0a2d5ae0ab18bbd2b
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-03-18 22:14:34 +0530

    Firebase Authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCLai69GlmaMGTptvR50p8oQUGFMMRKU7E",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:540004824315:web:ed4984aea51555cae32c5e",
   measurementId: "G-9EDRSFRYZ9",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
